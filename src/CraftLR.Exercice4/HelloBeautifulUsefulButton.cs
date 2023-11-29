b14f0924b2b85f5a74bf259dd81af68a48d78c81
Dragonkell <113989412+Dragonkell@users.noreply.github.com>
2023-11-29 15:12:37 +0100

    exercice 4

--- src/CraftLR.Exercice4/HelloBeautifulUsefulButton.cs
+++ src/CraftLR.Exercice4/HelloBeautifulUsefulButton.cs
@@ -2,6 +2,7 @@ using System;
 
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging; // Assurez-vous d'importer cette directive
 
 namespace CraftLR.Exercice4;
 
@@ -10,5 +11,20 @@ public partial class HelloBeautifulUsefulButton : Window
     public HelloBeautifulUsefulButton()
     {
         InitializeComponent();
+        LoadImage();
+    }
+
+    private void LoadImage()
+    {
+            var image = new Bitmap("Assets/logo.png");
+            var button = this.FindControl<Button>("Bouton1");
+            button.Content = new Image { Source = image };
+            button.Click += Button_OnClick;
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs eventData)
+    {
+        var button = this.FindControl<Button>("Bouton1");
+        button.Click += (sender, e) => Console.WriteLine("Bouton actionné");
     }
 }
